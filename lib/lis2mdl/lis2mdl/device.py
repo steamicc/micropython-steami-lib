e147cfc7769549d56bc94a3dd86fadcbcdeee038
Charly-sketch <glic.per@gmail.com>
2025-10-22 14:36:28 +0200

    lis2mdl: fix device configuration and first number.
    Still doesnt point to north but can see rotation

--- lib/lis2mdl/lis2mdl/device.py
+++ lib/lis2mdl/lis2mdl/device.py
@@ -1,27 +1,37 @@
+# device.py
 from machine import I2C
 from lis2mdl.const import *
 
 class LIS2MDL(object):
-    def __init__(self, i2c, address=LIS2MDL_I2C_ADDR):
+    def __init__(self, i2c, address=LIS2MDL_I2C_ADDR, odr_hz=10, temp_comp=True, low_power=False, drdy_pin=False):
         self.i2c = i2c
         self.address = address
-
         self.writebuffer = bytearray(1)
         self.readbuffer = bytearray(1)
 
-        # Initialize the device
-        self.reset()
-        self.configure()
+        # Soft reset property
+        self.setReg(0x20, LIS2MDL_CFG_REG_A)  # SOFT_RST=1 (not 0x10)
+        # small delay (if needed on the MicroPython side)
+        try:
+            import time; time.sleep_ms(10)
+        except:
+            pass
 
-    def reset(self):
-        # Reset the device using CFG_REG_A
-        self.setReg(0x10, LIS2MDL_CFG_REG_A)
+        # CFG_REG_A: COMP_TEMP, LP, ODR, MD=00 (continuous)
+        odr_bits = {10:0b00, 20:0b01, 50:0b10, 100:0b11}.get(odr_hz, 0b00)
+        comp = 1 if temp_comp else 0
+        lp   = 1 if low_power else 0
+        cfg_a = (comp<<7) | (lp<<4) | (odr_bits<<2) | 0b00
+        self.setReg(cfg_a, LIS2MDL_CFG_REG_A)   # <- essential to exit IDLE
 
-    def configure(self):
-        # Configure the device (example: enable continuous mode)
+        # CFG_REG_B: default 0x00 (LPF/off_canc off); you can enable LPF if desired
         self.setReg(0x00, LIS2MDL_CFG_REG_B)
-        self.setReg(0x01, LIS2MDL_CFG_REG_C)
 
+        # CFG_REG_C: BDU=1 (+ DRDY_on_PIN if requested)
+        cfg_c = 0x10 | (0x01 if drdy_pin else 0x00)
+        self.setReg(cfg_c, LIS2MDL_CFG_REG_C)
+
+    # --- identical low-level ---
     def setReg(self, data, reg):
         self.writebuffer[0] = data
         self.i2c.writeto_mem(self.address, reg, self.writebuffer)
@@ -30,22 +40,22 @@ class LIS2MDL(object):
         self.i2c.readfrom_mem_into(self.address, reg, self.readbuffer)
         return self.readbuffer[0]
 
-    def get2Reg(self, reg):
-        lowerByte = self.getReg(reg)
-        higherByte = self.getReg(reg + 1)
-        return (higherByte << 8) + lowerByte
+    # signed helper
+    @staticmethod
+    def _to_int16(v):
+        return v - 0x10000 if v & 0x8000 else v
 
+    # burst read + signed
     def read_magnet(self):
-        # Read magnetic field data for X, Y, Z axes
-        x = self.get2Reg(LIS2MDL_OUTX_L_REG)
-        y = self.get2Reg(LIS2MDL_OUTY_L_REG)
-        z = self.get2Reg(LIS2MDL_OUTZ_L_REG)
+        # auto-increment: reg | 0x80 (cf. datasheet I2C op)
+        buf = self.i2c.readfrom_mem(self.address, LIS2MDL_OUTX_L_REG | 0x80, 6)
+        x = self._to_int16((buf[1] << 8) | buf[0])
+        y = self._to_int16((buf[3] << 8) | buf[2])
+        z = self._to_int16((buf[5] << 8) | buf[4])
         return (x, y, z)
 
     def whoAmI(self):
-        # Read the WHO_AM_I register
         return self.getReg(LIS2MDL_WHO_AM_I)
 
     def status(self):
-        # Read the STATUS register
         return self.getReg(LIS2MDL_STATUS_REG)
