075158c487217af7d73500f7337fc3e338c6de61
Charly-sketch <glic.per@gmail.com>
2025-10-30 15:00:21 +0100

    lis2mdl: add LIS2MDL exemple.

---
+++ lib/lis2mdl/exemple/magnet_fieldForce.py
@@ -0,0 +1,13 @@
+# exemple of heading reading with direction label
+from time import sleep_ms
+from machine import I2C
+from lis2mdl.device import LIS2MDL
+from lis2mdl.const import *
+
+i2c = I2C(1)
+mag = LIS2MDL(i2c)
+
+while True:
+    field_strength = mag.magnitude_uT()
+    print("Champ magnétique :", field_strength, "µT")
+    sleep_ms(100)
