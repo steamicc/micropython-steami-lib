f335c038213eb7d354f869060c22216c13dd8262
Sébastien NEDJAR <sebastien@nedjar.com>
2026-03-14 15:20:37 +0100

    ism330dl: Apply project coding conventions to driver.

--- lib/ism330dl/ism330dl/const.py
+++ lib/ism330dl/ism330dl/const.py
@@ -1,78 +1,77 @@
-# ISM330DL constants
-
+from micropython import const
 
 # ---------------------------------------------------------------------
 # I2C addresses / identity
 # ---------------------------------------------------------------------
-ISM330DL_I2C_ADDR_LOW = 0x6A
-ISM330DL_I2C_ADDR_HIGH = 0x6B
-ISM330DL_WHO_AM_I_VALUE = 0x6A
+ISM330DL_I2C_ADDR_LOW = const(0x6A)
+ISM330DL_I2C_ADDR_HIGH = const(0x6B)
+ISM330DL_WHO_AM_I_VALUE = const(0x6A)
 
 
 # ---------------------------------------------------------------------
 # Registers
 # ---------------------------------------------------------------------
-REG_WHO_AM_I = 0x0F
-REG_CTRL1_XL = 0x10
-REG_CTRL2_G = 0x11
-REG_CTRL3_C = 0x12
-REG_STATUS_REG = 0x1E
+REG_WHO_AM_I = const(0x0F)
+REG_CTRL1_XL = const(0x10)
+REG_CTRL2_G = const(0x11)
+REG_CTRL3_C = const(0x12)
+REG_STATUS_REG = const(0x1E)
 
-REG_OUT_TEMP_L = 0x20
-REG_OUT_TEMP_H = 0x21
+REG_OUT_TEMP_L = const(0x20)
+REG_OUT_TEMP_H = const(0x21)
 
-REG_OUTX_L_G = 0x22
-REG_OUTX_H_G = 0x23
-REG_OUTY_L_G = 0x24
-REG_OUTY_H_G = 0x25
-REG_OUTZ_L_G = 0x26
-REG_OUTZ_H_G = 0x27
+REG_OUTX_L_G = const(0x22)
+REG_OUTX_H_G = const(0x23)
+REG_OUTY_L_G = const(0x24)
+REG_OUTY_H_G = const(0x25)
+REG_OUTZ_L_G = const(0x26)
+REG_OUTZ_H_G = const(0x27)
 
-REG_OUTX_L_XL = 0x28
-REG_OUTX_H_XL = 0x29
-REG_OUTY_L_XL = 0x2A
-REG_OUTY_H_XL = 0x2B
-REG_OUTZ_L_XL = 0x2C
-REG_OUTZ_H_XL = 0x2D
+REG_OUTX_L_XL = const(0x28)
+REG_OUTX_H_XL = const(0x29)
+REG_OUTY_L_XL = const(0x2A)
+REG_OUTY_H_XL = const(0x2B)
+REG_OUTZ_L_XL = const(0x2C)
+REG_OUTZ_H_XL = const(0x2D)
 
 
 # ---------------------------------------------------------------------
 # CTRL3_C bits
 # ---------------------------------------------------------------------
-CTRL3_C_BDU = 1 << 6
-CTRL3_C_IF_INC = 1 << 2
-CTRL3_C_SW_RESET = 1 << 0
+CTRL3_C_BDU = const(1 << 6)
+CTRL3_C_IF_INC = const(1 << 2)
+CTRL3_C_SW_RESET = const(1 << 0)
 
 
 # ---------------------------------------------------------------------
 # STATUS bits
 # ---------------------------------------------------------------------
-STATUS_TDA = 1 << 2
-STATUS_GDA = 1 << 1
-STATUS_XLDA = 1 << 0
+STATUS_TDA = const(1 << 2)
+STATUS_GDA = const(1 << 1)
+STATUS_XLDA = const(1 << 0)
 
 
 # ---------------------------------------------------------------------
 # Accelerometer ODR
 # ---------------------------------------------------------------------
-ACCEL_ODR_POWER_DOWN = 0x00
-ACCEL_ODR_12_5HZ = 0x01
-ACCEL_ODR_26HZ = 0x02
-ACCEL_ODR_52HZ = 0x03
-ACCEL_ODR_104HZ = 0x04
-ACCEL_ODR_208HZ = 0x05
-ACCEL_ODR_416HZ = 0x06
-ACCEL_ODR_833HZ = 0x07
-ACCEL_ODR_1660HZ = 0x08
+ACCEL_ODR_POWER_DOWN = const(0x00)
+ACCEL_ODR_12_5HZ = const(0x01)
+ACCEL_ODR_26HZ = const(0x02)
+ACCEL_ODR_52HZ = const(0x03)
+ACCEL_ODR_104HZ = const(0x04)
+ACCEL_ODR_208HZ = const(0x05)
+ACCEL_ODR_416HZ = const(0x06)
+ACCEL_ODR_833HZ = const(0x07)
+ACCEL_ODR_1660HZ = const(0x08)
 
 
 # ---------------------------------------------------------------------
 # Accelerometer full scale
 # ---------------------------------------------------------------------
-ACCEL_FS_2G = 2
-ACCEL_FS_4G = 4
-ACCEL_FS_8G = 8
-ACCEL_FS_16G = 16
+ACCEL_FS_2G = const(2)
+ACCEL_FS_4G = const(4)
+ACCEL_FS_8G = const(8)
+ACCEL_FS_16G = const(16)
 
 ACCEL_FS_BITS = {
     ACCEL_FS_2G: 0x00,
@@ -92,25 +91,25 @@ ACCEL_SENSITIVITY_MG = {
 # ---------------------------------------------------------------------
 # Gyroscope ODR
 # ---------------------------------------------------------------------
-GYRO_ODR_POWER_DOWN = 0x00
-GYRO_ODR_12_5HZ = 0x01
-GYRO_ODR_26HZ = 0x02
-GYRO_ODR_52HZ = 0x03
-GYRO_ODR_104HZ = 0x04
-GYRO_ODR_208HZ = 0x05
-GYRO_ODR_416HZ = 0x06
-GYRO_ODR_833HZ = 0x07
-GYRO_ODR_1660HZ = 0x08
+GYRO_ODR_POWER_DOWN = const(0x00)
+GYRO_ODR_12_5HZ = const(0x01)
+GYRO_ODR_26HZ = const(0x02)
+GYRO_ODR_52HZ = const(0x03)
+GYRO_ODR_104HZ = const(0x04)
+GYRO_ODR_208HZ = const(0x05)
+GYRO_ODR_416HZ = const(0x06)
+GYRO_ODR_833HZ = const(0x07)
+GYRO_ODR_1660HZ = const(0x08)
 
 
 # ---------------------------------------------------------------------
 # Gyroscope full scale
 # ---------------------------------------------------------------------
-GYRO_FS_125DPS = 125
-GYRO_FS_250DPS = 250
-GYRO_FS_500DPS = 500
-GYRO_FS_1000DPS = 1000
-GYRO_FS_2000DPS = 2000
+GYRO_FS_125DPS = const(125)
+GYRO_FS_250DPS = const(250)
+GYRO_FS_500DPS = const(500)
+GYRO_FS_1000DPS = const(1000)
+GYRO_FS_2000DPS = const(2000)
 
 GYRO_FS_BITS = {
     GYRO_FS_250DPS: 0x00,
