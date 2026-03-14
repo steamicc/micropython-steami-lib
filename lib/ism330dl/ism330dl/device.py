d050987d9f7ac252818998e634c98a68c73f786a
Sébastien NEDJAR <sebastien@nedjar.com>
2026-03-14 22:58:33 +0100

    ism330dl: Use polling with timeout in _ensure_data().

--- lib/ism330dl/ism330dl/device.py
+++ lib/ism330dl/ism330dl/device.py
@@ -137,7 +137,12 @@ class ISM330DL(object):
         if self._is_power_down():
             self.configure_accel(self._accel_odr, self._accel_scale)
             self.configure_gyro(self._gyro_odr, self._gyro_scale)
-            sleep_ms(100)
+            for _ in range(50):
+                s = self._read_u8(REG_STATUS_REG)
+                if s & STATUS_XLDA:
+                    return
+                sleep_ms(10)
+            raise OSError("ISM330DL data ready timeout")
 
     # --------------------------------------------------
     # Raw readings
