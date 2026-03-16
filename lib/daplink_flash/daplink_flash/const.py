597c975803255acf80217bc26ccd75991c685d82
Sébastien NEDJAR <sebastien@nedjar.com>
2026-03-16 14:25:17 +0100

    daplink_flash: Add read_sector and read methods.

--- lib/daplink_flash/daplink_flash/const.py
+++ lib/daplink_flash/daplink_flash/const.py
@@ -12,6 +12,7 @@ CMD_SET_FILENAME = const(0x03)
 CMD_GET_FILENAME = const(0x04)
 CMD_CLEAR_FLASH = const(0x10)
 CMD_WRITE_DATA = const(0x11)
+CMD_READ_SECTOR = const(0x20)
 
 # Registers
 REG_STATUS = const(0x80)
@@ -28,5 +29,6 @@ ERROR_CMD_FAILED = const(0x80)
 
 # Protocol limits
 MAX_WRITE_CHUNK = const(30)
+SECTOR_SIZE = const(256)
 FILENAME_LEN = const(8)
 EXT_LEN = const(3)
