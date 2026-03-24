2de4b1ba664eed9c9d32c888bb69b209324de532
Charly-sketch <glic.per@gmail.com>
2026-03-24 15:19:25 +0100

    style(wsen-pads): fix lint errors

--- lib/wsen-pads/examples/temp_pressure_display.py
+++ lib/wsen-pads/examples/temp_pressure_display.py
@@ -28,7 +28,7 @@ def bar_graph(value, vmin, vmax, width=20):
     return "[" + "#" * filled + "-" * (width - filled) + "]"
 
 while True:
-    pressure, temp = sensor.read() 
+    pressure, temp = sensor.read()
     temp_bar = bar_graph(temp, TEMP_MIN, TEMP_MAX)
     press_bar = bar_graph(pressure, PRESS_MIN, PRESS_MAX)
 
