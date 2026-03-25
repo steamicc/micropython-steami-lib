434c4fa39e93c0a955cc1dc83a6da3f888844a69
Charly-sketch <glic.per@gmail.com>
2026-03-25 09:52:34 +0100

    feat(wsen-hids): add dew point example

---
+++ lib/wsen-hids/examples/dew_point.py
@@ -0,0 +1,24 @@
+"""Compute and display the dew point temperature from humidity + temperature using the Magnus formula. Useful to understand when condensation might occur."""
+from math import log
+
+from machine import I2C
+from wsen_hids import WSEN_HIDS
+
+i2c = I2C(1)
+sensor = WSEN_HIDS(i2c)
+
+def dew_point_celsius(temperature_c, humidity):
+    # Magnus formula
+    a = 17.62
+    b = 243.12  # °C
+
+    gamma = (a * temperature_c / (b + temperature_c)) + log(humidity / 100.0)
+    dp = (b * gamma) / (a - gamma)
+    return dp
+
+humidity, temperature = sensor.read_one_shot()
+dew_point = dew_point_celsius(temperature, humidity)
+
+print("Humidity: {:.2f} %RH".format(humidity))
+print("Temperature: {:.2f} °C".format(temperature))
+print("Dew point: {:.2f} °C".format(dew_point))
