cc384ace56b84327791bb5a2f9220973a1deb0eb
Charly-sketch <glic.per@gmail.com>
2026-03-25 17:10:31 +0100

    fix(wsen-hids): Add sleep_ms ans forgotten titile line in datalogger example
    - Added a warning for the date erasing
    - Replace sleep by sleep_ms
    - writen head line for csv file

--- lib/wsen-hids/examples/data_logger.py
+++ lib/wsen-hids/examples/data_logger.py
@@ -1,5 +1,12 @@
-'''Read every 5s and print CSV-formatted output (timestamp, humidity, temperature) suitable for serial capture'''
-from time import sleep, ticks_diff, ticks_ms
+'''
+Read every 5s and print CSV-formatted output (timestamp, humidity, temperature) suitable for serial capture
+This example writes data both to the serial console and to DAPLink flash storage.
+
+WARNING: If configured to erase on startup, this script will erase the entire
+DAPLink flash contents before logging, which is a destructive operation.
+Make sure you have backed up any important data before enabling erasure.
+'''
+from time import sleep_ms, ticks_diff, ticks_ms
 
 from daplink_flash import DaplinkFlash
 from machine import I2C
@@ -12,12 +19,13 @@ flash = DaplinkFlash(i2c)
 # Set filename and erase
 flash.set_filename("data", "CSV")
 flash.clear_flash()
-sleep(0.5)
+sleep_ms(500)
 print("Flash erased.")
 
 start_ms = ticks_ms()
 
 print("timestamp,humidity,temperature")
+flash.write_line("timestamp,humidity,temperature")
 
 while True:
     humidity, temperature = sensor.read_one_shot()
@@ -26,4 +34,4 @@ while True:
     print("{},{:.2f},{:.2f}".format(elapsed_s, humidity, temperature))
     flash.write_line("{},{:.2f},{:.2f}".format(elapsed_s, humidity, temperature))
 
-    sleep(5)
+    sleep_ms(5000)
