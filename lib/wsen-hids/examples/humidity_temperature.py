8e3ba779011cecd453a2ff9382c447834d0938b5
Charly-sketch <glic.per@gmail.com>
2026-03-25 09:40:12 +0100

    feat(wsen-hids): add simple single read example

---
+++ lib/wsen-hids/examples/humidity_temperature.py
@@ -0,0 +1,14 @@
+from machine import I2C
+from wsen_hids import WSEN_HIDS
+
+# Initialise le bus I2C
+i2c = I2C(1)
+
+# Initialise le capteur
+sensor = WSEN_HIDS(i2c)
+
+# Lecture unique
+humidity, temperature = sensor.read_one_shot()
+
+print("Humidity: {:.2f} %RH".format(humidity))
+print("Temperature: {:.2f} °C".format(temperature))
