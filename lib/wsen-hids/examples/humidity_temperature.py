3f7ccd6e7f1b10d43ad2bcee4081e908c751cc6b
Charly-sketch <glic.per@gmail.com>
2026-03-25 09:53:04 +0100

    style(wsen-hids): remove french comment

--- lib/wsen-hids/examples/humidity_temperature.py
+++ lib/wsen-hids/examples/humidity_temperature.py
@@ -3,13 +3,10 @@
 from machine import I2C
 from wsen_hids import WSEN_HIDS
 
-# Initialise le bus I2C
 i2c = I2C(1)
 
-# Initialise le capteur
 sensor = WSEN_HIDS(i2c)
 
-# Lecture unique
 humidity, temperature = sensor.read_one_shot()
 
 print("Humidity: {:.2f} %RH".format(humidity))
