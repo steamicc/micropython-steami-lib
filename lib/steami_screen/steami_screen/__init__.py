02f4d70229b70611404f6521c5bc59f14ac124f6
Charly-sketch <glic.per@gmail.com>
2026-04-02 09:38:05 +0200

    feat(steami_screen): Add SSD1327 and GC9A01 display wrappers
    with example for analog clock

--- lib/steami_screen/steami_screen/__init__.py
+++ lib/steami_screen/steami_screen/__init__.py
@@ -1,4 +1,5 @@
 from steami_screen.colors import rgb_to_gray4, rgb_to_rgb8, rgb_to_rgb565
+from steami_screen.gc9a01 import GC9A01Display
 from steami_screen.screen import (
     BLACK,
     BLUE,
@@ -11,6 +12,7 @@ from steami_screen.screen import (
     YELLOW,
     Screen,
 )
+from steami_screen.sssd1327 import SSD1327Display
 
 __all__ = [
     "BLACK",
@@ -22,6 +24,8 @@ __all__ = [
     "RED",
     "WHITE",
     "YELLOW",
+    "GC9A01Display",
+    "SSD1327Display",
     "Screen",
     "rgb_to_gray4",
     "rgb_to_rgb8",
