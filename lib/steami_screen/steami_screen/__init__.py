a21ebef0ba258ff1e24edf20b15f5cbdfd4064d5
MatteoCnda1 <matteodaniel13960@gmail.com>
2026-09-23 09:45:48 +0200

    refactor(steami_screen): move constants to dedicated const.py file (#424).

--- lib/steami_screen/steami_screen/__init__.py
+++ lib/steami_screen/steami_screen/__init__.py
@@ -1,22 +1,24 @@
 from steami_screen.colors import rgb_to_gray4, rgb_to_rgb8, rgb_to_rgb565
-from steami_screen.device import (
+from steami_screen.const import (
     BLACK,
     BLUE,
     DARK,
+    FACES,
     GRAY,
     GREEN,
     LIGHT,
     RED,
     WHITE,
     YELLOW,
-    Screen,
 )
+from steami_screen.device import Screen
 from steami_screen.ssd1327 import SSD1327Display
 
 __all__ = [
     "BLACK",
     "BLUE",
     "DARK",
+    "FACES",
     "GRAY",
     "GREEN",
     "LIGHT",
